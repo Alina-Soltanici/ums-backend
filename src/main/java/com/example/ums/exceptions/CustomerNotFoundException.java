853b4c41b80e886587b73a0f9028d029a6e1de66
Alina <soltanicialina777@gmail.com>
2026-01-15 16:47:05 +0200

    add CustomerNotFoundException custom exception

---
+++ src/main/java/com/example/ums/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.ums.exceptions;
+
+public class CustomerNotFoundException extends RuntimeException {
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
