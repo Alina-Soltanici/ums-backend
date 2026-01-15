4bdfa4edf4b0783d1a71fd7d37a3ef408ba2eaf6
Alina <soltanicialina777@gmail.com>
2026-01-15 21:34:53 +0200

    Add @Builder annotation to LoginResponse DTO for easier object creation

--- src/main/java/com/example/ums/dto/LoginResponse.java
+++ src/main/java/com/example/ums/dto/LoginResponse.java
@@ -1,9 +1,11 @@
 package com.example.ums.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 
 @Data
+@Builder
 public class LoginResponse {
     private Long id;
     private String accessToken;
