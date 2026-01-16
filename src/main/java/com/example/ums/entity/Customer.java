3aeefa8a10045b797ea53a0c5f2e88acc6619e95
Alina <soltanicialina777@gmail.com>
2026-01-16 08:20:59 +0200

    add +@AllArgsConstructor and @Builder annotations

--- src/main/java/com/example/ums/entity/Customer.java
+++ src/main/java/com/example/ums/entity/Customer.java
@@ -1,11 +1,15 @@
 package com.example.ums.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
 @NoArgsConstructor
+@AllArgsConstructor
 @Data
 @Table(name = "customers", schema = "auth")
 public class Customer {
